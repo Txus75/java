97136b0c239bb91f47a806b5b28152bc0d3ed7f4
Txus75 <txus75h@yahoo.es>
2019-10-11 18:50:00 +0200

    prueba-spring
    Primera prueba fallida con spring

---
+++ semana5/spring-prueba/src/com/trifulcas/springprueba/Saludo.java
@@ -0,0 +1,13 @@
+package com.trifulcas.springprueba;
+
+public class Saludo {
+
+	public String hola() {
+		return "Hola ¿Qué tal?";
+	}
+	
+	public String adios() {
+		return "Adios, que te vaya bien.";
+	}
+	
+}
